39d3a04fb2c1d7f231d24dcc37dad1c4a5ba5546
ErDiCooper <ericdicarlo@hotmail.com>
2019-07-14 16:04:45 -0600

    Finished(?) the Stack class while studying for Exam 2.

--- Stack.java
+++ Stack.java
@@ -1,4 +1,80 @@
-public class Stack {
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+
+   private Node<T> topNode;
+   
+   public Stack () {
+      topNode = null;
+   } // End of default constructor.
+   
+   
+   //*****************************************************************************************
+      
+      /**
+       * Push - adds a new Node to the top of the Stack.
+       *
+       * @param newEntry - The data to be stored inside the Stack.
+       */
+   public void push (T newEntry) {
+      Node newNode = new Node(newEntry, topNode);
+      topNode = newNode;
+   } // End of push.
+   
+   //*****************************************************************************************
+      
+      /**
+       * Peek - Shows the user the top Node in the Stack.
+       *
+       * @return - The data from inside of the top Node.
+       */   
+   public T peek() {
+      if(isEmpty()) {
+         throw new EmptyStackException();
+      }
+      else {
+         return topNode.getData();
+      }
+   } // End of peek.
+   
+   //*****************************************************************************************
+      
+      /**
+       * Pop - Removes the top Node from the Stack.
+       *
+       * @param top - The data from inside of the removed Node.
+       */
+   public T pop() {
+      T top = peek();
+      
+      topNode = topNode.getNext();
+      
+      return top;
+   } // End of pop.
+   
+   //*****************************************************************************************
+      
+      /**
+       * isEmpty - Checks to see if the Stack is currently empty.
+       *
+       * @return - A boolean response to whether or not the Stack is empty.
+       */
+   public boolean isEmpty() {
+      return topNode == null;
+   } // End of isEmpty.
+   
+   //*****************************************************************************************
+      
+      /**
+       * Clear - sets the top Node to null, effectively clearing the Stack.
+       */
+   public void clear() {
+      topNode = null;
+   } // End of clear.
+
+
+
+
 
 //*****************************************************************************************
 //*****************************************************************************************
